102a66622f659654829e3fb2a72ff54d20c552ab
bakseck <seck.bakar@ugb.edu.sn>
2024-01-18 01:41:25 +0000

    correct

--- js/graph.js
+++ js/graph.js
@@ -1,36 +1,94 @@
-const svgNS = "http://www.w3.org/2000/svg";
+function genererGraphique(data) {
+    const svg = document.getElementById('xp');
+    const svgWidth = svg.clientWidth;
+    const svgHeight = 350;
+    const barWidth = 10;
+    const xOffset = 20;
+    const barGap = 5;
+    const xp_max = Math.max(...data.map(item => item.xp));
+    const yAxisLength = svgHeight + 10;
+    const xAxisLength = data.length * (barWidth + barGap) + 50;
 
-function createSvgElement(type, attributes) {
-    const element = document.createElementNS(svgNS, type);
-    for (const attr in attributes) {
-        if (attributes.hasOwnProperty(attr)) {
-            element.setAttribute(attr, attributes[attr]);
-        }
-    }
-    return element;
+    let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    applyStyle(text, {
+        "x": "300",
+        "y": "50",
+        "fill": "#947fbd",
+        "font-size": "24px",
+        "font-family": "Arial, sans-serif"
+    });
+    text.textContent = "Projects Done";
+
+    svg.appendChild(text);
+
+    // Dessiner l'axe Y
+    const yAxis = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    applyStyle(yAxis, {
+        "x1": xOffset.toString(),
+        "y1": "5",
+        "x2": xOffset.toString(),
+        "y2": yAxisLength.toString(),
+        "stroke": "#4D4D4D"
+    });
+    svg.appendChild(yAxis);
+
+    // Dessiner l'axe X
+    const xAxis = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    applyStyle(xAxis, {
+        "x1": xOffset.toString(),
+        "y1": yAxisLength.toString(),
+        "x2": xAxisLength.toString(),
+        "y2": yAxisLength.toString(),
+        "stroke": "#4d4d4d"
+    });
+    svg.appendChild(xAxis);
+
+    data.forEach((item, index) => {
+
+        let barHeight = (item.xp / xp_max) * svgHeight;
+        let bar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+        let barStyle = {
+            "class": "barre",
+            "x": 20 + (barWidth + barGap) * index,
+            "y": 10 + svgHeight - barHeight,
+            "width": barWidth,
+            "height": barHeight
+        };
+
+        applyStyle(bar, barStyle);
+
+        // Ajouter un événement de survol pour afficher une infobulle
+        bar.onmouseover = function () {
+            showTooltip(`${item.name} - ${item.xp} XP`, event);
+        };
+        bar.onmouseout = function () {
+            hideTooltip();
+        };
+
+        svg.appendChild(bar);
+    });
 }
 
-function createTooltip() {
+function applyStyle(element, styles) {
+    for (let property in styles) {
+        element.setAttribute(property, styles[property]);
+    }
+}
+
+function showTooltip(text, event) {
     let tooltip = document.getElementById('tooltip');
     if (!tooltip) {
         tooltip = document.createElement('div');
         tooltip.id = 'tooltip';
-        Object.assign(tooltip.style, {
-            position: 'absolute',
-            padding: '5px',
-            background: 'black',
-            color: 'white',
-            borderRadius: '4px',
-            opacity: '0.75',
-            visibility: 'hidden'
-        });
+        tooltip.style.position = 'absolute';
+        tooltip.style.padding = '5px';
+        tooltip.style.background = 'black';
+        tooltip.style.color = 'white';
+        tooltip.style.borderRadius = '4px';
+        tooltip.style.opacity = '0.75';
         document.body.appendChild(tooltip);
     }
-    return tooltip;
-}
-
-function showTooltip(text, event) {
-    const tooltip = createTooltip();
     tooltip.textContent = text;
     tooltip.style.left = `${event.clientX}px`;
     tooltip.style.top = `${event.clientY}px`;
@@ -44,96 +102,64 @@ function hideTooltip() {
     }
 }
 
-function genererGraphique(data) {
-    const svg = document.getElementById('xp');
-    const svgWidth = svg.clientWidth;
-    const svgHeight = 350;
-    const barWidth = 10;
-    const xOffset = 20;
-    const barGap = 5;
-    const xp_max = Math.max(...data.map(item => item.xp));
-
-    svg.appendChild(createSvgElement('text', {
-        x: "300",
-        y: "50",
-        fill: "#947fbd",
-        "font-size": "24px",
-        "font-family": "Arial, sans-serif",
-        textContent: "Projects Done"
-    }));
-
-    svg.appendChild(createSvgElement('line', {
-        x1: xOffset.toString(),
-        y1: "5",
-        x2: xOffset.toString(),
-        y2: (svgHeight + 10).toString(),
-        stroke: "#4D4D4D"
-    }));
-
-    svg.appendChild(createSvgElement('line', {
-        x1: xOffset.toString(),
-        y1: (svgHeight + 10).toString(),
-        x2: (data.length * (barWidth + barGap) + 50).toString(),
-        y2: (svgHeight + 10).toString(),
-        stroke: "#4d4d4d"
-    }));
-
-    data.forEach((item, index) => {
-        const barHeight = (item.xp / xp_max) * svgHeight;
-        const bar = createSvgElement('rect', {
-            class: "barre",
-            x: 20 + (barWidth + barGap) * index,
-            y: 10 + svgHeight - barHeight,
-            width: barWidth,
-            height: barHeight
-        });
-
-        bar.onmouseover = function (event) {
-            showTooltip(`${item.name} - ${item.xp} XP`, event);
-        };
-        bar.onmouseout = hideTooltip;
-
-        svg.appendChild(bar);
-    });
-}
-
 function drawCirculaireDiagram(data) {
-    const svg = document.getElementById("audit-ratio");
+    const svgNS = "http://www.w3.org/2000/svg";
+    const svg = document.getElementById("audit-ratio")
 
     const centerX = 200;
     const centerY = 200;
     const radius = 100;
+
     let startAngle = 0;
 
-    svg.appendChild(createSvgElement('text', {
-        x: "120",
-        y: "50",
-        fill: "#947fbd",
+    let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    applyStyle(text, {
+        "x": "120",
+        "y": "50",
+        "fill": "#947fbd",
         "font-size": "24px",
-        "font-family": "Arial, sans-serif",
-        textContent: "Audit Ratio"
-    }));
+        "font-family": "Arial, sans-serif"
+    });
+    text.textContent = "Audit Ratio";
+
+    svg.appendChild(text);
 
     data.forEach(item => {
-        const angle = (item.value / 100) * Math.PI * 2;
+        const { label, value, color } = item;
+
+        const angle = (value / 100) * Math.PI * 2;
         const endAngle = startAngle + angle;
 
-        const path = createSvgElement('path', {
-            d: [
-                `M ${centerX} ${centerY}`,
-                `L ${centerX + radius * Math.cos(startAngle)} ${centerY + radius * Math.sin(startAngle)}`,
-                `A ${radius} ${radius} 0 ${angle > Math.PI ? 1 : 0} 1 ${centerX + radius * Math.cos(endAngle)} ${centerY + radius * Math.sin(endAngle)}`,
-                "Z"
-            ].join(" "),
-            fill: item.color
-        });
+        const x1 = centerX + radius * Math.cos(startAngle);
+        const y1 = centerY + radius * Math.sin(startAngle);
+        const x2 = centerX + radius * Math.cos(endAngle);
+        const y2 = centerY + radius * Math.sin(endAngle);
 
-        path.onmouseover = (event) => showTooltip(`${item.label} ${item.value.toFixed(2)} %`, event);
-        path.onmouseout = hideTooltip;
+        const path = document.createElementNS(svgNS, "path");
+        const largeArcFlag = angle > Math.PI ? 1 : 0;
+        const d = [
+            `M ${centerX} ${centerY}`,
+            `L ${x1} ${y1}`,
+            `A ${radius} ${radius} 0 ${largeArcFlag} 1 ${x2} ${y2}`,
+            "Z"
+        ].join(" ");
+
+        path.setAttribute("d", d);
+        path.setAttribute("fill", color);
+
+        path.onmouseover = function (event) {
+            showTooltip(`${label} ${value.toFixed(2)} %`, event);
+        };
+
+        path.onmouseout = function () {
+            hideTooltip();
+        };
 
         svg.appendChild(path);
+
         startAngle = endAngle;
     });
+
 }
 
-export { genererGraphique, drawCirculaireDiagram };
+export { genererGraphique, applyStyle, drawCirculaireDiagram };
