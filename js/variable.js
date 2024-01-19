9b3a6cd2e231a7126a401501bd093db0e10ba595
bakseck <seck.bakar@ugb.edu.sn>
2024-01-19 10:45:14 +0000

    reset

--- js/variable.js
+++
@@ -1,6 +0,0 @@
-const DOMAIN = "learn.zone01dakar.sn"
-const API_URL = `https://${DOMAIN}/api/graphql-engine/v1/graphql`
-const URL_SIGNIN = `https://${DOMAIN}/api/auth/signin`
-
-
-export {API_URL, URL_SIGNIN, DOMAIN};
